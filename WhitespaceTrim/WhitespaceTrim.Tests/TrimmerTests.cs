8ceaa7951f8c327216c14cb0f8b6894847ef1396
Simon Headley <simon.headley@adaptit.co.za>
2019-07-03 09:11:47 +0200

    removed nested repository and fixed it to be a single repo

---
+++ WhitespaceTrim/WhitespaceTrim.Tests/TrimmerTests.cs
@@ -0,0 +1,78 @@
+﻿using NUnit.Framework;
+
+namespace WhitespaceTrim.Tests
+{
+    [TestFixture]
+    public class TrimmerTests
+    {
+        [Test]
+        public void Trim_GivenAbc_ShouldReturnAbc()
+        {
+            // Arrange
+            var input = "abc";
+            var expectedOutput = "abc";
+
+            // Act
+            var actualOutput = Trimmer.Trim(input);
+
+            // Assert
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+
+        [Test]
+        public void Trim_GivenAbcSpace_ShouldReturnAbc()
+        {
+            // Arrange
+            var input = "abc ";
+            var expectedOutput = "abc";
+
+            // Act
+            var actualOutput = Trimmer.Trim(input);
+
+            // Assert
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+
+        [Test]
+        public void Trim_GivenAbcBacktickT_ShouldReturnAbc()
+        {
+            // Arrange
+            var input = "abc\t";
+            var expectedOutput = "abc";
+
+            // Act
+            var actualOutput = Trimmer.Trim(input);
+
+            // Assert
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+
+        [Test]
+        public void Trim_GivenSpaceAbc_ShouldReturnAbc()
+        {
+            // Arrange
+            var input = " abc";
+            var expectedOutput = " abc";
+
+            // Act
+            var actualOutput = Trimmer.Trim(input);
+
+            // Assert
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+
+        [Test]
+        public void Trim_GivenComplexStringWithSpacesInBetween_ShouldReturnStringWithoutSpaces()
+        {
+            // Arrange
+            var input = "ab\r\n cd \r\n";
+            var expectedOutput = "ab\r\ncd\r\n";
+
+            // Act
+            var actualOutput = Trimmer.Trim(input);
+
+            // Assert
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+    }
+}
---
+++ WhitespaceTrim/WhitespaceTrim/Trimmer.cs
@@ -0,0 +1,20 @@
+﻿namespace WhitespaceTrim
+{
+    public static class Trimmer
+    {
+        public static string Trim(string input)
+        {
+            var output = input[0].ToString();
+
+            for (var i = 1; i < input.Length; i++)
+            {
+                if (input[i] != ' ' && input[i] != '\t')
+                {
+                    output += input[i];
+                }
+            }
+
+            return output;
+        }
+    }
+}
